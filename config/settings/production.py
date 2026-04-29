fbca2f39d9c9ac80e730f4064fab0202b75267ad
azizurrahamangithub <mdhridoy01646185824@gmail.com>
2026-04-29 09:33:19 +0600

    cors error fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -29,6 +29,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 ALLOWED_HOSTS = ['31.220.60.125', 'localhost', '127.0.0.1']
 
 CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
